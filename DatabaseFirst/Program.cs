b0ab3da78227b27b13bee2c8d47658b8810f95f8
sayan saha <sayan199783@gmail.com>
2023-01-04 18:36:42 +0530

    InitialCommit

---
+++ DatabaseFirst/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst
+{
+    class Program
+    {
+        public static PlutoContext context = new PlutoContext();
+        static void Main(string[] args)
+        {
+            // Call a stored procedure
+            var courses = context.GetCourses();
+            foreach(var c in courses)
+            {
+                Console.WriteLine(c.Description);
+            }
+            //Use of Enum
+            var course = new Course();
+            course.Level = CourseLevel.Intermediatte;
+        }
+    }
+}
